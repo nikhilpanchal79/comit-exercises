2bb3cb8c87c01d65e8ebb1622889827d613eede5
Nikhil Panchal <nikhil.pmp@gmail.com>
2020-09-23 08:02:25 -0500

    Ex 19 created

---
+++ index.js
@@ -0,0 +1,7 @@
+const MONTH_OF_BIRTH = "August";
+const FIRST_NAME = "Nikhil";
+const LAST_NAME = "Panchal";
+let city = "Winnipeg";
+let age = 30;
+let nextCity = city;
+console.log("Month of Birth: ", MONTH_OF_BIRTH);
