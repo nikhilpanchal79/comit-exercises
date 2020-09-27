b87331151815627b59428aabdea7a8fbeaf20bd4
Nikhil Panchal <nikhil.pmp@gmail.com>
2020-09-27 14:27:28 -0500

    Portfolio created.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict";
 
-for (let index = 0; index < 10; index++) {
-  console.log("*".repeat(index));
+let valid = prompt("What is 1+2+3+4+5+6+7+8+9+10? 🤔 Think hard please!",0);
+
+if (valid == 55) {
+    alert("Welcome to Nikhil's portfolio.");
+} else {
+    alert("Improve your maths skills. Good bye!");
+    window.location.replace("https://google.ca");
 }
