658035f4995c732141576e7b320f26d91cb656d3
Nikhil Panchal <nikhil.pmp@gmail.com>
2020-09-28 18:40:39 -0500

    Portfolio Update 2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-"use strict";
+// "use strict";
 
-let valid = prompt("What is 1+2+3+4+5+6+7+8+9+10? 🤔 Think hard please!",0);
+// let valid = Number(prompt("What is 1+2+3+4+5+6+7+8+9+10? 🤔 Think hard please!",0));
 
-if (valid == 55) {
-    alert("Welcome to Nikhil's portfolio.");
-} else {
-    alert("Improve your maths skills. Good bye!");
-    window.location.replace("https://google.ca");
-}
+// if (valid == 55) {
+//     alert("Welcome to Nikhil's portfolio.");
+// } else {
+//     alert("Improve your maths skills. Good bye!");
+//     window.location.replace("https://google.ca");
+// }
